cf261a6aec24c14a7bd00d7238ca49d7ac3280f3
alexsha <alexsha@wix.com>
2019-05-23 20:56:58 +0300

    add aes encryption example

---
+++ AesEncrypt.cs
@@ -0,0 +1,18 @@
+public static string Encrypt(string str) {
+ using(var aes = new AesManaged()) {
+  HashAlgorithm algorithm = SHA1.Create();
+  aes.Key = algorithm.ComputeHash(Encoding.UTF8.GetBytes(secretKey)).Take(16).ToArray();
+  aes.Mode = CipherMode.ECB;
+  aes.Padding = PaddingMode.PKCS7;
+
+  using(var ms = new MemoryStream()) {
+   using(var cs = new CryptoStream(ms, aes.CreateEncryptor(), CryptoStreamMode.Write)) {
+    byte[] data = Encoding.UTF8.GetBytes(str);
+    cs.Write(data, 0, data.Length);
+   }
+
+   byte[] encrypted = ms.ToArray();
+   return Convert.ToBase64String(encrypted).Replace('+', '-').Replace('/', '_').Replace("=", "");;
+  }
+ }
+}
