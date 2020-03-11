b96cb09fc2517189253577f634660846716b9aca
alexshawix <40203566+alexshawix@users.noreply.github.com>
2020-03-11 09:51:51 +0200

    Update AesEncrypt.cs

--- AesEncrypt.cs
+++ AesEncrypt.cs
@@ -1,24 +1,82 @@
+using Newtonsoft.Json;
 using System;
 using System.IO;
+using System.Linq;
 using System.Security.Cryptography;
+using System.Text;
 
+namespace Answers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string ssoCallbackUrl = "<answers sso callback url>";
+            string keyId = "<your api key id>";
+            string secret = "<your api key secret>";
 
-public static string Encrypt(string str) {
-    
-    using(var aes = new AesManaged()) {
-        HashAlgorithm algorithm = SHA1.Create();
-        aes.Key = algorithm.ComputeHash(Encoding.UTF8.GetBytes(secretKey)).Take(16).ToArray();
-        aes.Mode = CipherMode.ECB;
-        aes.Padding = PaddingMode.PKCS7;
+            UserData userData = new UserData
+            {
+                Id = "<your internal id>",
+                Email = "<user email>",
+                Timestamp = DateTimeOffset.UtcNow.ToUnixTimeMilliseconds()
+            };
 
-        using(var ms = new MemoryStream()) {
-            using(var cs = new CryptoStream(ms, aes.CreateEncryptor(), CryptoStreamMode.Write)) {
-                byte[] data = Encoding.UTF8.GetBytes(str);
-                cs.Write(data, 0, data.Length);
+            string jsonUserData = JsonConvert.SerializeObject(userData);
+
+            string token = GenerateEncryptedToken(jsonUserData, secret);
+
+            string redirectUrl = $"{ssoCallbackUrl}&token={token}&key={keyId}";
+
+            Console.WriteLine(redirectUrl);
+        }
+
+        public static string GenerateEncryptedToken(string json, string secret)
+        {
+
+            using (var aes = new AesManaged())
+            {
+                HashAlgorithm algorithm = SHA1.Create();
+                aes.Key = algorithm.ComputeHash(Encoding.UTF8.GetBytes(secret)).Take(16).ToArray();
+                aes.Mode = CipherMode.ECB;
+                aes.Padding = PaddingMode.PKCS7;
+
+                using (var ms = new MemoryStream())
+                {
+                    using (var cs = new CryptoStream(ms, aes.CreateEncryptor(), CryptoStreamMode.Write))
+                    {
+                        byte[] data = Encoding.UTF8.GetBytes(json);
+                        cs.Write(data, 0, data.Length);
+                    }
+
+                    byte[] encrypted = ms.ToArray();
+                    return Convert.ToBase64String(encrypted).Replace('+', '-').Replace('/', '_').Replace("=", "");
+                }
             }
+        }
 
-            byte[] encrypted = ms.ToArray();
-            return Convert.ToBase64String(encrypted).Replace('+', '-').Replace('/', '_').Replace("=", "");
+        public class UserData
+        {
+            [JsonProperty("id", Required = Required.Always)]
+            public string Id { get; set; }
+
+            [JsonProperty("email", Required = Required.Always)]
+            public string Email { get; set; }
+
+            [JsonProperty("timestamp", Required = Required.Always)]
+            public long Timestamp { get; set; }
+
+            [JsonProperty("firstName")]
+            public String FirstName { get; set; }
+
+            [JsonProperty("lastName")]
+            public String LastName { get; set; }
+
+            [JsonProperty("displayName")]
+            public String DisplayName { get; set; }
+
+            [JsonProperty("profileImage")]
+            public String ProfileImage { get; set; }
         }
     }
 }
